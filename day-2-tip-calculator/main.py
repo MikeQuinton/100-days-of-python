c58571643bef306fe6763994b662228f059f19d8
Michael Quinton <32737081+MikeQuinton@users.noreply.github.com>
2024-02-21 19:45:55 +0000

    Initial commit

---
+++ day-2-tip-calculator/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator")
+bill = float(input("What was the total bill? "))
+tip = int(input("What percentage tip would you like to give (10, 12, or 15)? "))
+people = int(input ("How many people to split the bill? "))
+total = bill / people * (tip / 100 + 1)
+print(f"£{total: .2f}")
